6b73abb98f72f791fada6c38b71829aceddedfd5
Robert Mariuson <gigglefeet@gmail.com>
2021-01-23 17:48:26 +0100

    Add final version2

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const request = require('request');
 const fs = require('fs');
 const axios = require('axios').default;
 const path = require('path');
-
 // the axios method returns the data from the meme image when console logged.
 axios
   .get('https://api.memegen.link/images/puffin.jpg?watermark=none')
@@ -13,14 +12,12 @@ axios
     // Handle Error Here
     console.error(err);
   });
-
 fs.mkdir(path.join(__dirname, 'memes'), { recursive: true }, (err) => {
   if (err) {
     return console.error(err);
   }
   console.log('Directory created successfully!');
 });
-
 const array = [];
 // Request for the URL from our meme example site.
 request(
@@ -30,7 +27,6 @@ request(
     if (err) {
       return console.log(err);
     }
-
     // this is how the img tag is extracted to the variable re
     const re = /<img[^>]+src="([^">]+)"/g;
     // A for loop that goes through the first 10 meme img tags, why it does that I have no Idea! but it does.
@@ -38,12 +34,10 @@ request(
       // This peace of code I don't understand yet
       let results = re.exec(res.body);
       let source = results[1];
-
       array.push(results[1]);
       //console.log(results);
     }
     console.log(array);
-
     // A for loop that loops through the array starting at i = 0, and loops through the size of the array ".lenght"
     for (let i = 0; i < array.length; i++) {
       console.log(i);
