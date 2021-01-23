616edbf4fc2f30d1de877b6336a925482370d838
Robert Mariuson <gigglefeet@gmail.com>
2021-01-23 14:19:40 +0100

    Add almost finished2

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const request = require('request');
 const fs = require('fs');
+const axios = require('axios').default;
+const path = require('path');
+
+// the axios method returns the data from the meme image when console logged.
+axios
+  .get('https://api.memegen.link/images/puffin.jpg?watermark=none')
+  .then((resp) => {
+    // console.log(resp.data);
+  })
+  .catch((err) => {
+    // Handle Error Here
+    console.error(err);
+  });
+
+fs.mkdir(path.join(__dirname, 'memes'), { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Directory created successfully!');
+});
 
 const array = [];
 // Request for the URL from our meme example site.
@@ -18,8 +38,18 @@ request(
       // This peace of code I don't understand yet
       let results = re.exec(res.body);
       let source = results[1];
+
       array.push(results[1]);
+      //console.log(results);
     }
     console.log(array);
+    for (let i = 0; i < array.length; i++) {
+      console.log(i);
+      console.log(array[i]);
+      fs.writeFileSync(
+        `${__dirname}/memes/img_${i + 1}.jpg`,
+        array.toString('binary'),
+      );
+    }
   },
 );
